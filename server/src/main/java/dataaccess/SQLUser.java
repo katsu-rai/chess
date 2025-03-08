87dd238d26e355ed06e53e28b2aa7ec5718305a6
katsu-rai <taradancan0603@gmail.com>
2025-03-08 10:06:59 -0700

    Added SQLUser constructor to SQLUser and springframework dependency

--- server/src/main/java/dataaccess/SQLUser.java
+++ server/src/main/java/dataaccess/SQLUser.java
@@ -1,8 +1,33 @@
 package dataaccess;
 
 import model.UserData;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.sql.SQLException;
 
 public class SQLUser implements UserDAO {
+
+    public SQLUser(){
+        try {
+            DatabaseManager.createDatabase();
+        } catch (DataAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        try (var connection = DatabaseManager.getConnection()) {
+            var createTestTable = """            
+                CREATE TABLE if NOT EXISTS user (
+                                username VARCHAR(50) NOT NULL,
+                                password VARCHAR(50) NOT NULL,
+                                email VARCHAR(100),
+                                PRIMARY KEY (username)
+                                )""";
+            try (var createTableStatement = connection.prepareStatement(createTestTable)) {
+                createTableStatement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public UserData getUser(String username) throws DataAccessException{
