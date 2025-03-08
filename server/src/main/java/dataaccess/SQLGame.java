df5cc15000067107f4ea3bc8b497ac71cc2063c8
Rai Katsuragawa <taradancan0603@gmail.com>
2025-03-08 07:22:49 -0700

    Added SQLGame constructor and getAllGames methods in SQLGame.java

--- server/src/main/java/dataaccess/SQLGame.java
+++ server/src/main/java/dataaccess/SQLGame.java
@@ -1,38 +1,95 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.GameData;
 
 import java.util.HashSet;
+import java.sql.*;
+import com.google.gson.Gson;
 
 public class SQLGame implements GameDAO {
 
-    @Override
-    public HashSet<GameData> getAllGames(){
-        return new HashSet<>();
+    public SQLGame() {
+        try {
+            DatabaseManager.createDatabase();
+        } catch (DataAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        try (var connection = DatabaseManager.getConnection()) {
+            var createTestTable = """            
+                CREATE TABLE if NOT EXISTS game (
+                                gameID INT NOT NULL,
+                                whiteUsername VARCHAR(50),
+                                blackUsername VARCHAR(50),
+                                gameName VARCHAR(50),
+                                chessGame TEXT,
+                                PRIMARY KEY (gameID)
+                                )""";
+            try (var createTableStatement = connection.prepareStatement(createTestTable)) {
+                createTableStatement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void addGame(GameData game) throws DataAccessException{
+    public HashSet<GameData> getAllGames() {
+        HashSet<GameData> resultGames = new HashSet<>();
+        String sql = "SELECT * FROM game";
+
+        try (
+                Connection connection = DatabaseManager.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                resultGames.add(new GameData(
+                        rs.getInt("gameID"),
+                        rs.getString("whiteUsername"),
+                        rs.getString("blackUsername"),
+                        rs.getString("gameName"),
+                        deserializeGame(rs.getString("chessGame"))
+                ));
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        return resultGames;
+    }
+
+    @Override
+    public void addGame(GameData game) throws DataAccessException {
 
     }
 
     @Override
-    public GameData getGame(int id) throws DataAccessException{
+    public GameData getGame(int id) throws DataAccessException {
         return null;
     }
 
     @Override
-    public void clear(){
+    public void clear() {
 
     }
 
     @Override
-    public int getMaxId(){
+    public int getMaxId() {
         return 0;
     }
 
     @Override
-    public void updateGame(GameData game) throws DataAccessException{
+    public void updateGame(GameData game) throws DataAccessException {
 
+    }
+
+    private String serializeGame(ChessGame game) {
+        return new Gson().toJson(game);
+    }
+
+    private ChessGame deserializeGame(String serializedGame) {
+        return new Gson().fromJson(serializedGame, ChessGame.class);
     }
 }
