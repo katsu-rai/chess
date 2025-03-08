b9d12b81585309f937e89d900417f53696d2302c
Rai Katsuragawa <taradancan0603@gmail.com>
2025-03-08 06:15:16 -0700

    Added getAuth to SQLAuth.java

--- server/src/main/java/dataaccess/SQLAuth.java
+++ server/src/main/java/dataaccess/SQLAuth.java
@@ -3,6 +3,7 @@ package dataaccess;
 import model.AuthData;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SQLAuth implements AuthDAO {
@@ -45,12 +46,30 @@ public class SQLAuth implements AuthDAO {
 
     @Override
     public void deleteAuth(String authToken){
-
+        try (Connection connection = DatabaseManager.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("DELETE FROM auth WHERE authToken = ?")) {
+                statement.setString(1, authToken);
+                statement.executeUpdate();
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException{
+        try (Connection connection = DatabaseManager.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("SELECT FROM auth WHERE authToken = ?")) {
+                statement.setString(1, authToken);
 
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    String username =  resultSet.getString("username");
+                    return new AuthData(authToken, username);
+                }
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new DataAccessException("This AuthToken does not exist: " + authToken);
+        }
     }
 
     @Override
